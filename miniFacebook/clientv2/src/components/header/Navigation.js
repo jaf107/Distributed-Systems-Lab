eecb8e2c6fc98f188b07ce95f5880fc99f811509
jaf107 <48786629+jaf107@users.noreply.github.com>
2022-07-19 21:29:24 +0600

    Navigation fixed

--- miniFacebook/clientv2/src/components/header/Navigation.js
+++ miniFacebook/clientv2/src/components/header/Navigation.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from 'reactstrap'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux';
-class Navigation extends Component {
-    isAuthenticatedUser = () => {
-        const { isAuthenticated } = useSelector((state) => state.user.isAuthenticated)
-        return isAuthenticated;
+import { connect } from 'react-redux';
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.user.isAuthenticated,
+        // orderLoading: state.orderLoading,
+        // orderErr: state.orderErr,
     }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        // fetchOrders: () => dispatch(fetchOrders()),
+    }
+}
+class Navigation extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             isNavOpen: false
         }
-        // this.selector = 
+
     }
     navToggle = () => {
         this.setState({
@@ -20,9 +30,45 @@ class Navigation extends Component {
         })
     }
     render() {
-        // const { isAuthenticated } = useSelector((state) => state.user.isAuthenticated)
-        var i = this.isAuthenticatedUser();
-        console.log(i);
+        // console.log(this.props.isAuthenticated)
+        let routes = null;
+        if (this.props.isAuthenticated === true) {
+            routes = (
+                <Nav className='mr-auto' navbar>
+                    <NavItem>
+                        <Link to="/" className='nav-link active'>Home</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/story" className='nav-link '>Story</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/status" className='nav-link'>Status</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/dashboard" className='nav-link'>Dashboard</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/logout" className='nav-link'>Logout</Link>
+                    </NavItem>
+                </Nav>
+            )
+        } else {
+            routes = (
+                <Nav className='mr-auto' navbar>
+                    <NavItem>
+                        <Link to="/" className='nav-link active'>Home</Link>
+                    </NavItem>
+
+                    <NavItem>
+                        <Link to="/login" className='nav-link'>Login</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/register" className='nav-link'>Register</Link>
+                    </NavItem>
+                    
+                </Nav>
+            )
+        }
         return (
             <div>
                 <Navbar dark color='dark' expand='sm'>
@@ -30,23 +76,7 @@ class Navigation extends Component {
                         <NavbarToggler onClick={this.navToggle} />
                         <NavbarBrand href='/'>miniFacebook</NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav className='mr-auto' navbar>
-                                <NavItem>
-                                    <Link to="/" className='nav-link active'>Home</Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link to="/story" className='nav-link '>Story</Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link to="/status" className='nav-link'>Status</Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link to="/login" className='nav-link'>Login</Link>
-                                </NavItem>
-                                <NavItem>
-                                    <Link to="/register" className='nav-link'>Register</Link>
-                                </NavItem>
-                            </Nav>
+                            {routes}
                         </Collapse>
                     </div>
                 </Navbar>
@@ -54,4 +84,4 @@ class Navigation extends Component {
         )
     }
 }
-export default Navigation
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation)
